adb34eca9dec67ba9e371101f755bb7122a1d6a8
joepklaassen <joep.klaassen9@gmail.com>
2024-02-28 14:24:36 +0100

    Started on auth

--- app.js
+++ app.js
@@ -16,31 +16,6 @@ app.listen(port, () => {
 // Enable CORS for all routes
 app.use(cors());
 
-// Connect to the DB
-async function connect() {
-  try {
-    await mongoose.connect(uri);
-    console.log("Connected to MongoDB");
-    test = true;
-  } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
-    test = false;
-  }
-}
-connect();
-
-async function insertTestUsers() {
-  await UserModel.insertMany([
-    { username: "john_doe", password: "123", email: "john.doe@example.com" },
-    {
-      username: "jane_smith",
-      password: "123",
-      email: "jane.smith@example.com",
-    },
-  ]);
-  console.log('Data inserted into "users" collection');
-}
-
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -55,8 +30,53 @@ app.get("/users", (req, res) => {
     });
 });
 
+// Register User
+app.post("/register", async (req, res) => {
+  try {
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    // Create a new user document
+    const user = new UserModel({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+    // Save the user to the database
+    await user.save();
+    res.status(201).send("User registered successfully");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error registering user");
+  }
+});
+
 // Export the Express API
 module.exports = app;
+
+// async function insertTestUsers() {
+//   await UserModel.insertMany([
+//     { username: "john_doe", password: "123", email: "john.doe@example.com" },
+//     {
+//       username: "jane_smith",
+//       password: "123",
+//       email: "jane.smith@example.com",
+//     },
+//   ]);
+//   console.log('Data inserted into "users" collection');
+// }
+
+// // Connect to the DB
+// async function connect() {
+//   try {
+//     await mongoose.connect(uri);
+//     console.log("Connected to MongoDB");
+//     test = true;
+//   } catch (error) {
+//     console.error("Error connecting to MongoDB:", error);
+//     test = false;
+//   }
+// }
+// connect();
 
 /** Routes */
 // const userRoutes = require('./routes/user/userRoutes');
